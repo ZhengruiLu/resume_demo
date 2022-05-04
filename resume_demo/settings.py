e4ee337f3ad0c4d2939123a23a926b6204a37533
ZhengruiLu <lu.zhengr@northeastern.edu>
2022-05-03 17:04:42 -0700

    update for adding walkthrough.gif

--- resume_demo/settings.py
+++ resume_demo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-34gc-0$fjtyaq-119cq+y#svx$2bvlm_r7yzp6^%#b_!^9t*7k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 # 'django-env.eba-athkfpib.us-west-2.elasticbeanstalk.com'
